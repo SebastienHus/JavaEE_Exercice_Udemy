77e4b27b1d3aa6e07e94a61cef46392d745a57cf
pirato <muchhus@gmail.com>
2019-12-10 04:18:22 +0100

    exercice 3 section 4 cour java EE

---
+++ exercice_3_section4/frontOffice/src/main/java/com/directmedia/onlineStore/frontOffice/controller/HomeServlet.java
@@ -0,0 +1,62 @@
+package com.directmedia.onlineStore.frontOffice.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<!DOCTYPE html>\r\n" + 
+				"\r\n" + 
+				"<html>\r\n" + 
+				"\r\n" + 
+				"\r\n" + 
+				"\r\n" + 
+				"<head>\r\n" + 
+				"\r\n" + 
+				"<meta charset=\"utf-8\">\r\n" + 
+				"\r\n" + 
+				"<title>Online Store</title>\r\n" + 
+				"\r\n" + 
+				"</head>\r\n" + 
+				"\r\n" + 
+				"<!--  -->\r\n" + 
+				"\r\n" + 
+				"<body>\r\n" + 
+				"\r\n" + 
+				"<header>\r\n" + 
+				"\r\n" + 
+				"<h1>Online Store</h1>\r\n" + 
+				"\r\n" + 
+				"<p>\"Votre boutique multimedia en ligne\"</p>\r\n" + 
+				"\r\n" + 
+				"</header>\r\n" + 
+				"\r\n" + 
+				"</body>\r\n" + 
+				"\r\n" + 
+				"</html>");
+	}
+
+
+}
